2326c6378fe074263ae543903556c8e3e157fe6a
Andrebacklund123 <backlund.andre@hotmail.com>
2024-09-22 23:17:43 +0200

    Update  service-worker.js
    Signed-off-by: Andrebacklund123 <backlund.andre@hotmail.com>

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+{
+  "name": "Aktivitetsmeny",
+  "short_name": "Aktivitetsmeny",
+  "description": "En app för att utforska olika aktiviteter och nivåer på Ullevi Tennisklubb",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#0d47a1",
+  "icons": [
+    {
+      "src": "/apple-touch-icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/apple-touch-icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
